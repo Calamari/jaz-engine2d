775e3760664250f5f96ccc6d706a2d7fb5415633
Georg Tavonius <georg.tavonius@xing.com>
2012-05-15 18:36:10 +0200

    adding test example for viewing collision in the browser

--- libs/test.js
+++ libs/test.js
@@ -1,6 +1,3 @@
-//= require vector
-//= require collision_detection
-//= require collidable
 
 var requestAnimFrame = (function() {
   return window.requestAnimationFrame
@@ -20,9 +17,50 @@ window.addEventListener('DOMContentLoaded', function() {
   canvasElement.width = window.innerWidth;
   canvasElement.height = window.innerHeight;
 
+  window.rect = new Collidable.Rectangle({
+    position: new Vector(50, 50),
+    width: 100,
+    height: 100,
+  });
+  window.rect2 = new Collidable.Rectangle({
+    position: new Vector(170, 50),
+    width: 100,
+    height: 100,
+  });
+  window.poly1 = new Collidable.Polygon({
+    position: new Vector(50, 300),
+    points: [new Vector(0, 0), new Vector(40, 20), new Vector(0, 40)]
+  });
+  window.poly2 = new Collidable.Polygon({
+    position: new Vector(80, 300),
+    points: [new Vector(0, 20), new Vector(40, 0), new Vector(0, 40)]
+  });
+
+  var drawPolygon = function(poly) {
+    context.strokeStyle = poly.isHit ? '#ff0000' : '#000000';
+    context.beginPath();
+    for (var i=0,l=poly.points.length; i<l; ++i) {
+      context.moveTo(poly.position.x + poly.points[i].x, poly.position.y + poly.points[i].y);
+      var nextPoint = poly.points[i===l-1 ? 0 : i+1];
+      context.lineTo(poly.position.x + nextPoint.x, poly.position.y + nextPoint.y);
+    }
+    context.stroke();
+  };
+
+  var collisionDetection = new CollisionDetection(rect, rect2);
+
   var render = function() {
+    collisionDetection.test();
     context.clearRect(0,0, canvasElement.width, canvasElement.height);
 
+    context.strokeStyle = rect.isHit ? '#ff0000' : '#000000';
+    context.strokeRect(rect.position.x, rect.position.y, rect.width, rect.height);
+
+    context.strokeStyle = rect2.isHit ? '#ff0000' : '#000000';
+    context.strokeRect(rect2.position.x, rect2.position.y, rect2.width, rect2.height);
+
+    drawPolygon(poly1);
+    drawPolygon(poly2);
 
     requestAnimFrame(render);
   };
