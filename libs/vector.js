de7524092b2ec892d6541b83bbeb6dff72231cf4
calamari <g.tavonius@gmail.com>
2013-01-13 10:54:16 +0100

    added method rotationBetween, and put function around

--- libs/vector.js
+++ libs/vector.js
@@ -1,86 +1,95 @@
-/*jslint node: true */
-"use strict";
+/*globals module */
+(function(win) {
+  "use strict";
 
-/**
- *  An implementation of a vector
- * @TODO: fasten up with prototype
- *
- * benchmarks in: http://jsperf.com/vector2d
- */
-var Vector = function(x, y) {
-  this.x = x ? x : 0;
-  this.y = y ? y : 0;
-};
+  /**
+   *  An implementation of a vector
+   * @TODO: fasten up with prototype
+   *
+   * benchmarks in: http://jsperf.com/vector2d
+   */
+  var Vector = function(x, y) {
+    this.x = x ? x : 0;
+    this.y = y ? y : 0;
+  };
 
-Vector.prototype.clone = function() {
-  return new Vector(this.x, this.y);
-};
+  Vector.prototype.clone = function() {
+    return new Vector(this.x, this.y);
+  };
 
-Vector.prototype.add = function(v) {
-  this.x += v.x;
-  this.y += v.y;
-  return this;
-};
+  Vector.prototype.add = function(v) {
+    this.x += v.x;
+    this.y += v.y;
+    return this;
+  };
 
-Vector.prototype.sub = function(v) {
-  this.x -= v.x;
-  this.y -= v.y;
-  return this;
-};
+  Vector.prototype.sub = function(v) {
+    this.x -= v.x;
+    this.y -= v.y;
+    return this;
+  };
 
-Vector.prototype.dot =
-Vector.prototype.dotProduct = function(v) {
-  return this.x * v.x + this.y * v.y;
-};
+  Vector.prototype.dot =
+  Vector.prototype.dotProduct = function(v) {
+    return this.x * v.x + this.y * v.y;
+  };
 
-/** returns the right hand normal of vector */
-Vector.prototype.rightNormal = function() {
-  return new Vector(-this.y, this.x);
-};
+  /** returns the right hand normal of vector */
+  Vector.prototype.rightNormal = function() {
+    return new Vector(-this.y, this.x);
+  };
 
-/** returns the left hand normal of vector */
-Vector.prototype.leftNormal = function() {
-  return new Vector(this.y, -this.x);
-};
+  /** returns the left hand normal of vector */
+  Vector.prototype.leftNormal = function() {
+    return new Vector(this.y, -this.x);
+  };
 
-Vector.prototype.skalar = function(a) {
-  this.x *= a;
-  this.y *= a;
-  return this;
-};
+  Vector.prototype.skalar = function(a) {
+    this.x *= a;
+    this.y *= a;
+    return this;
+  };
 
-Vector.prototype.length = function() {
-  return this.distanceTo({ x: 0, y: 0 });
-};
+  Vector.prototype.length = function() {
+    return this.distanceTo({ x: 0, y: 0 });
+  };
 
-Vector.prototype.distanceFrom =
-Vector.prototype.distanceTo = function(v) {
-  return Math.sqrt(Math.pow(this.x - v.x,2) + Math.pow(this.y - v.y,2));
-};
+  Vector.prototype.distanceFrom =
+  Vector.prototype.distanceTo = function(v) {
+    return Math.sqrt(Math.pow(this.x - v.x,2) + Math.pow(this.y - v.y,2));
+  };
 
-Vector.prototype.equals = function(v) {
-  return this.x === v.x && this.y === v.y;
-};
+  Vector.prototype.equals = function(v) {
+    return this.x === v.x && this.y === v.y;
+  };
 
-Vector.prototype.toJSON = function() {
-  return { x: this.x, y: this.y };
-};
+  Vector.prototype.toJSON = function() {
+    return { x: this.x, y: this.y };
+  };
 
-Vector.prototype.normalize = function(toLength) {
-  if (typeof toLength === 'undefined') { toLength = 1; }
-  this.skalar(toLength / this.length());
-  return this;
-};
+  Vector.prototype.normalize = function(toLength) {
+    if (typeof toLength === 'undefined') { toLength = 1; }
+    this.skalar(toLength / this.length());
+    return this;
+  };
 
-Vector.prototype.rotate = function(degree) {
-  degree = degree * (Math.PI / 180.0);
-  var nx = Math.cos(degree) * this.x - Math.sin(degree) * this.y,
-      ny = Math.sin(degree) * this.x + Math.cos(degree) * this.y;
-  this.x = nx;
-  this.y = ny;
-  return this;
-};
+  Vector.prototype.rotate = function(degree) {
+    degree = degree * (Math.PI / 180.0);
+    var nx = Math.cos(degree) * this.x - Math.sin(degree) * this.y,
+        ny = Math.sin(degree) * this.x + Math.cos(degree) * this.y;
+    this.x = nx;
+    this.y = ny;
+    return this;
+  };
 
-if (typeof module !== 'undefined') {
-  module.exports = Vector;
-}
+  Vector.prototype.rotationBetween = function(v) {
+    var cosAngle = this.dot(v) / (this.length() * v.length());
+    return Math.acos(cosAngle) * 180/Math.PI;
+  };
+
+  if (typeof module !== 'undefined') {
+    module.exports = Vector;
+  } else {
+    win.Vector = Vector;
+  }
+}(window));
